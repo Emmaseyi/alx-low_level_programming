9de62dca97871fd048b0fdae4fd2ecaaa57318e4
Emmanuel <emmaseyi93@yahoo.com>
2022-09-08 15:58:17 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+* Return: 1
+*/
+int main(void)
+{
+putchar(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+return (1);
+}
