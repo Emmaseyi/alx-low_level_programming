c3de74221d10e0d234b831c8ac40d6ae480e67bf
Emmanuel <emmaseyi93@yahoo.com>
2022-09-08 16:21:15 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
+
 #include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - print the string in the put function
+* main - print quote
 *
-* Description: using the main function
-* this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+* Description: print Dora's quote
 * Return: 1
 */
 int main(void)
 {
-putchar(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
 return (1);
 }
