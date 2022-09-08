74fd568ecbd777d8211ea1b27a0ad8faea594e6b
Emmanuel <emmaseyi93@yahoo.com>
2022-09-08 08:42:04 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash                                                                                                                     
-#include <stdio.h>                                                                                                              
-                                                                                                                                
-/**                                                                                                                             
-* main - print the string in the put function                                                                                   
-*                                                                                                                               
-* Description: using the main function                                                                                          
-* this program prints "Programming is like building a multilingual puzzle                                                       
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
