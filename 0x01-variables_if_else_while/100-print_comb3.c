dfcd8abd19ad1751df652321cafd69b0f0a25a97
Emmanuel <emmaseyi93@yahoo.com>
2022-09-10 14:55:09 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,28 +6,26 @@
  */
 int main(void)
 {
-	int a = 0;
-	int b;
-	int c;
+int a = 0;
+int b;
+int c;
 
-	while (a <= 99)
-	{
-		b = (a / 10 + '0');
-		c = (a % 10 + '0');
-
-		if (b < c)
-		{
-			putchar(b);
-			putchar(c);
-
-			if (a != 89)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		a++;
-	}
+while (a <= 99)
+{
+b = (a / 10 + '0');
+c = (a % 10 + '0');
+if (b < c)
+{
+putchar(b);
+putchar(c);
+if (a != 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+a++;
+}
 putchar('\n');
 return (0);
 }
