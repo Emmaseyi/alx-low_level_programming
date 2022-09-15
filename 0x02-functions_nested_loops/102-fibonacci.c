49f9732fd0de03f3de425be787d9b526bf7b6df4
Emmanuel <emmaseyi93@yahoo.com>
2022-09-14 18:52:07 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+/**
+ * main - prints out first 50
+ * fibonacci suit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+	unsigned long f1 = 0, f2 = 1, f3;
+
+	for (d = 0; d < 50; d++)
+	{
+		f3 = f1 + f2;
+	printf("%lu", f3);
+	f1 = f2;
+	f2 = f3;
+
+	if (d == 49)
+	printf("\n")
+	else
+	printf(", ");
+
+	}
+	return (0);
+}
