93878e7100446393698e1528e7c6bc99a1350024
Emmanuel <emmaseyi93@yahoo.com>
2022-09-21 18:14:55 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,21 +15,21 @@ int i = 0, diff = 0;
 while (i)
 {
 if (s1[i] == '\0' && s2[i] == '\0')
-	break;
+break;
 else if (s1[i] == '\0')
 {
-	diff = s2[i];
-	break;
+diff = s2[i];
+break;
 }
 else if (s2[i] == '\0')
 {
-	diff = s1[i];
-	break;
+diff = s1[i];
+break;
 }
 else if (s1[i] != s2[i])
 {
-	diff = s1[i] - s2[i];
-	break;
+diff = s1[i] - s2[i];
+break;
 }
 else
 i++;
