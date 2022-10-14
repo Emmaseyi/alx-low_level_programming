acfdcf75e113d86d3bd618305cfb0bb94b8bce4f
Emmanuel <emmaseyi93@yahoo.com>
2022-10-14 04:46:49 -0700

    Task all

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,11 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * sum_them_all-returns the sum of all its parameters
- * @separator:the string to be printed between the strings
- * @n:the number of strings passed to the function
- * Return:If separator is NULL, don’t print it
- * 	If one of the string is NULL, print (nil)
+ * print_strings - prints strings, followed by a new line.
+ * @separator: separator to print between the strings
+ * @n: number of strings to print
+ *
+ * Return: void
  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list valist;
 
-int sum_them_all(const unsigned int n, ...);
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(valist, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,19 +2,16 @@
 #define _VARIADIC_FUNCTIONS_H_
 
 #include <stdarg.h>
-
 /**
- * struct print-print type with corresponding print func.
- * @t:primt type
- * @f:print function
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
  */
-
 typedef struct print
 {
 	char *t;
 	void (*f)(va_list);
-}
-print_t;
+} print_t;
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
