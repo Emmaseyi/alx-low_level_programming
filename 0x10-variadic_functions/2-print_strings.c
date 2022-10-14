06294a6c8a693a551935dc5dd995526d65156104
Emmanuel <emmaseyi93@yahoo.com>
2022-10-13 17:59:19 -0700

    Task 0

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all-returns the sum of all its parameters
+ * @separator:the string to be printed between the strings
+ * @n:the number of strings passed to the function
+ * Return:If separator is NULL, don’t print it
+ * 	If one of the string is NULL, print (nil)
+ */
+
+int sum_them_all(const unsigned int n, ...);
